6a01e6a0c20469a18e3b842fc439acdb6cc7d8f2
zxy-lgtm <1563925525@qq.com>
2021-05-09 10:28:11 +0800

    11.2,11.3

---
+++ csapp11/11.2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <unistd.h>
+
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+int main (void)
+
+{
+char IPdotdec[20]; //存放点分十进制IP地址
+
+struct in_addr s; // IPv4地址结构体
+
+// 输入IP地址
+
+printf("Please input IP address: ");
+
+scanf("%s", IPdotdec);
+
+// 转换
+
+inet_pton(AF_INET, IPdotdec, (void *)&s);//该函数得到的字节顺序为主机字节顺序，即小端顺序
+
+//在这里调用了函数htonl将字节顺序换成了网络字节顺序
+s.s_addr = htonl(s.s_addr);
+printf("inet_pton: 0x%x\n", s.s_addr);
+
+// 反转换
+//在这里调用了函数ntonl将字节顺序换成了主机字节顺序
+s.s_addr = ntohl(s.s_addr);
+inet_ntop(AF_INET, (void *)&s, IPdotdec, 16);
+
+printf("inet_ntop: %s\n", IPdotdec);
+
+}
+
+
